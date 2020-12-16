65ce1c07d439c2e5874360043bfe02c507b49ce2
M. Rothe <rothe@internet-sicherheit.de>
2020-12-16 14:16:12 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['pive', 'pive/visualization'],
     use_2to3=True,
     include_package_data=True,
-    version='0.3.202012151430',
+    version='0.3.202012161415',
     url='https://github.com/daboth/pive',
     download_url='https://github.com/daboth/pive/tarball/0.3.4',
     license='BSD',
