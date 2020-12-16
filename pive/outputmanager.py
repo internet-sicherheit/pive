e166baa767c1c011aaf08f6031ba900073eb35b2
M. Rothe <rothe@internet-sicherheit.de>
2020-12-16 13:14:24 +0100

    Added finer support for selecting output paths

--- pive/outputmanager.py
+++ pive/outputmanager.py
@@ -9,21 +9,39 @@ class AbstractOutputManager(ABC):
         raise NotImplemented()
 
 
-class FolderOutputManager(AbstractOutputManager):
+class SimpleFolderOutputManager(AbstractOutputManager):
 
     def __init__(self, output_path):
         self.output_path = Path(output_path).resolve()
+
+    def get_output_folder_path(self, key, options):
+        return self.output_path
+
 
     def output(self, data, **options):
         for key in data:
             self.output_path.mkdir(exist_ok=True)
             if 'filenames' in options:
                 filename = options['filenames'].get(key, key)
+                if filename is None:
+                    # Explicitly skip this file
+                    continue
             else:
                 filename = key
             #TODO: Process options for writing here
-            with self.output_path.joinpath(filename).open(mode="w") as outfile:
+            with self.get_output_folder_path(key, options).joinpath(filename).open(mode="w") as outfile:
                 outfile.write(data[key])
+
+class FolderOutputManager(SimpleFolderOutputManager):
+    """Similar to SimpleFolderOutputManager, but makes it possible to change the folder on a per-file basis"""
+
+    def get_output_folder_path(self, key, options):
+        if 'folders' not in options:
+            #Use default output path, works the same as SimpleFolderOutputManager
+            return self.output_path
+        else:
+            return Path(options['folders'].get(key, self.output_path))
+
 
 class NullOutputManager(AbstractOutputManager):
 
